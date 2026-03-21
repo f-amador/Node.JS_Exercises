1af6ef387c673bdefebd303911379b9f60e353ca
f-amador <framador99@gmail.com>
2026-03-21 15:21:41 +0000

    Managed to do the github authentication

--- Auth/createuser.sh
+++ Auth/createuser.sh
@@ -1,3 +1,5 @@
-curl -X POST http://localhost:3000/register \
-  -H "Content-Type: application/json" \
-  -d '{"email":"user@example.com","password":"secret123","name":"Test User"}' | jq .
+curl -s -X POST http://localhost:3000/register -H 'Content-Type: application/json' -d '{"email":"a@a.com","password":"pass123","name":"Ada"}'
+
+curl -s -X POST http://localhost:3000/login -H 'Content-Type: application/json' -d '{"email":"a@a.com","password":"pass123"}'
+
+curl -s http://localhost:3000/profile -H "Authorization: Bearer YOUR_TOKEN_HERE"
